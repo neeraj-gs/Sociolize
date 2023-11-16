ed06a0df3500c003a44c77f5140807a4ef16995f
neeraj-gs <gsneeraj05@gmail.com>
2023-11-16 18:57:42 +0530

    INstelled react icons and also initlised a page for grid

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,15 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>Hii</div>
+    <main>
+      <div className='grid grid-cols-12 h-screen w-screen'>
+        <div className='col-span-3'></div>
+        <div className='col-span-6 border-r-2 border-l-2 border-slate-200'></div>
+        <div className='col-span-3'></div>
+      </div>
+    </main>
   )
 }
