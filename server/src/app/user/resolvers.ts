46c73903808ab092d399c6ebb34558a67c0ef171
neeraj-gs <gsneeraj05@gmail.com>
2023-12-01 12:38:13 +0530

    Added a interface for the gogole token res we get from the google api

--- server/src/app/user/resolvers.ts
+++ server/src/app/user/resolvers.ts
@@ -1,5 +1,28 @@
 import axios from "axios";
 
+interface GoogleTokenResult{
+    iss?:string;
+    nbf?:string;
+    aud?:string;
+    sub?:string;
+    email:string;
+    email_verified:string;
+    azp?:string;
+    name?:string;
+    picture?:string;
+    given_name?:string;
+    family_name?:string;
+    iat?:string;
+    exp?:string;
+    jti?:string;
+    alg?:string;
+    kid?:string;
+    typ?:string;
+}
+
+
+
+
 const queries = {
     verifyGoogleToken: async(parent:any,{token}:{token:string})=>{
         const googleToken = token;
