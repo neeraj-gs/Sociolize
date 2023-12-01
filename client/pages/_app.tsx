0d1ee5345df79c9dd378bd627bd78bb7f4481b9c
neeraj-gs <gsneeraj05@gmail.com>
2023-12-01 12:06:40 +0530

    Added a consent screena dna dded clietn id into our projct snad wrapped it with gogle oauth proider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return <div className={inter.className}>
-    <Component {...pageProps} />
+    <GoogleOAuthProvider clientId='878508288770-bo8t81sj1p1v759ms7un30c54euf2iug.apps.googleusercontent.com'>
+      <Component {...pageProps} />
+    </GoogleOAuthProvider>
   </div> 
 }
