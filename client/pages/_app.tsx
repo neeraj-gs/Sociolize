42a0d51db5f635f2201f2e5fd6cf5f490e09056e
neeraj-gs <gsneeraj05@gmail.com>
2023-12-01 13:35:54 +0530

    ADded codegen and added code to verify from cleint side, need to fix the network request failed bug

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { Toaster } from 'react-hot-toast'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return <div className={inter.className}>
     <GoogleOAuthProvider clientId='878508288770-bo8t81sj1p1v759ms7un30c54euf2iug.apps.googleusercontent.com'>
       <Component {...pageProps} />
+      <Toaster />
     </GoogleOAuthProvider>
   </div> 
 }
