f39f2f884fed9ae5703d88e41663f50905ea3c9e
neeraj-gs <gsneeraj05@gmail.com>
2023-12-04 18:39:27 +0530

    Added a Dynamic page for the User Profile Page and maintained teh layout of the page

---
+++ client/pages/[id].tsx
@@ -0,0 +1,15 @@
+import Layout from '@/components/Layout/Layout'
+import type {NextPage} from 'next'
+
+const UserProfilePage:NextPage=()=>{
+    return(
+        <div>
+            <Layout>
+                <h1>Profile Page</h1>
+            </Layout>
+        </div>
+    )
+}
+
+export default UserProfilePage
+
