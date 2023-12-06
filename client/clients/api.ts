2b1483f2368012b89dd00c6e2b08157decb514d4
neeraj-gs <gsneeraj05@gmail.com>
2023-12-06 21:31:56 +0530

    Added files to store teh deployed graphql server at the ec2 instance using cloudflare for gettginhttsp

--- client/clients/api.ts
+++ client/clients/api.ts
@@ -3,7 +3,7 @@ import {GraphQLClient} from 'graphql-request'
 const isClient = typeof window !== 'undefined' //it is a typecheck , if server side renderd lcient is false, if cleint send token elase dont send
 
 
-export const graphqlClient = new GraphQLClient('https://d1rb2bxxlg2ohh.cloudfront.net/graphql',{
+export const graphqlClient = new GraphQLClient(process.env.NEXT_PUBLIC_API_URL as string,{
     headers:()=>({
         Authorization:isClient ? 
         `Bearer ${window.localStorage.getItem("__sociolize_token")}`
